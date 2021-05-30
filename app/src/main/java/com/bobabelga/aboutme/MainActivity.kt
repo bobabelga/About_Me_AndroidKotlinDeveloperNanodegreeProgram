4680775df8b38856464e1e205015b7a340114bf2
belgacem abibakr <belgacemabibakr@gmail.com>
2021-05-30 20:51:01 +0100

    update

--- app/src/main/java/com/bobabelga/aboutme/MainActivity.kt
+++ app/src/main/java/com/bobabelga/aboutme/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.bobabelga.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.bobabelga.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.apply {
+            doneBtn.setOnClickListener {
+                nickNameTxt.setText(nickNameEdit.text)
+                nickNameEdit.visibility = View.GONE
+                it.visibility = View.GONE
+                nickNameTxt.visibility = View.VISIBLE
+
+                (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                    .hideSoftInputFromWindow(view.windowToken,0)
+            }
+        }
     }
 }
